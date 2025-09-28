55d574dae56c3976475b157b59c62e8ec12919a1
Juani <juanignaciog17@gmail.com>
2025-09-28 13:19:41 -0300

    feat(header): implement responsive sidebar navigation and update header styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,45 +1,137 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./css/Header.module.css";
+import breakpoints from "../../constants/breakpoints";
 
-const Header: React.FC = () => {
+interface SidebarProps {
+    showSidebar: boolean;
+    handleSidebar: () => void;
+}
+
+const Sidebar: React.FC<SidebarProps> = (props) => {
+    const { showSidebar, handleSidebar } = props;
+
     return (
-        <header id={styles.header}>
-            <nav id={styles.nav}>
-                <div>
-                    <a className={styles.navLink} href="/">
-                        <img id={styles.mainLogo} src="/src/assets/logos/main-logo.svg" />
-                    </a>
-                    <ol>
-                        <li>
-                            <a className={styles.navLink} href="#aboutMe" title="About me">
-                                <img src="/src/assets/icons/aboutme.svg" />
-                            </a>
-                        </li>
-                        <li>
-                            <a className={styles.navLink} href="#" title="Skills">
-                                <img src="/src/assets/icons/skills.svg" />
-                            </a>
-                        </li>
-                        <li>
-                            <a className={styles.navLink} href="#" title="Projects">
-                                <img src="/src/assets/icons/projects.svg" />
-                            </a>
-                        </li>
-                        <li>
-                            <a className={styles.navLink} href="#" title="Contact">
-                                <img src="/src/assets/icons/contact.svg" />
-                            </a>
-                        </li>
-                    </ol>
-                </div>
+        <nav className={`${styles.verticalNav} ${showSidebar ? styles.closeVerticalNav : {}}`}>
+            <div id={styles.sidebarTop}>
+                <ul id={styles.verticalNavList}>
+                    <li id={styles.iconCloseItem}>
+                        <img id={styles.iconClose} src="/src/assets/icons/close.svg" onClick={handleSidebar} />
+                    </li>
+                    <li>
+                        <a className={styles.navLink} href="#aboutMe" title="About me">
+                            About me
+                        </a>
+                    </li>
+                    <li>
+                        <a className={styles.navLink} href="#skills" title="Skills">
+                            Skills
+                        </a>
+                    </li>
+                    <li>
+                        <a className={styles.navLink} href="#projects" title="Projects">
+                            Projects
+                        </a>
+                    </li>
+                    <li>
+                        <a className={styles.navLink} href="#contact" title="Contact">
+                            Contact
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div id={styles.sidebarBottom}>
                 <div className={styles.switchContainer}>
                     <label className={styles.switch}>
                         <input type="checkbox" />
                         <span className={styles.slider}></span>
                     </label>
                 </div>
-            </nav>
-        </header>
+            </div>
+        </nav>
+    );
+}
+
+const Header: React.FC = () => {
+    const [showSidebar, setShowSidebar] = useState<boolean>(false);
+    const [renderSidebar, setRenderSidebar] = useState<boolean>(false);
+    const [screenWidth, setScreenWidth] = useState<number>(window.innerWidth);
+
+    const handleSidebar = () => {
+        setShowSidebar((prev) => !prev);
+    }
+
+    useEffect(() => {
+        const handleResize = () => setScreenWidth(window.innerWidth);
+
+        window.addEventListener("resize", handleResize);
+
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    useEffect(() => {
+        if (screenWidth < breakpoints.MD) {
+            setRenderSidebar(true);
+        } else {
+            setRenderSidebar(false)
+        }
+    }, [screenWidth]);
+
+    return (
+        <>
+            <header id={styles.header}>
+                <nav id={styles.horizontalNav}>
+                    <a id={styles.headerLeftSide} href="/">
+                        <img id={styles.mainLogo} src="/src/assets/logos/main-logo.svg" />
+                        <p id={styles.logoFullname}>Juan Ignacio Giusiano</p>
+                    </a>
+                    <div id={styles.headerRightSide}>
+                        {
+                            !renderSidebar && (
+                                <>
+                                    <ul id={styles.navList}>
+                                        <li>
+                                            <a className={styles.navLink} href="#aboutMe" title="About me">
+                                                About me
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a className={styles.navLink} href="#skills" title="Skills">
+                                                Skills
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a className={styles.navLink} href="#projects" title="Projects">
+                                                Projects
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a className={styles.navLink} href="#contact" title="Contact">
+                                                Contact
+                                            </a>
+                                        </li>
+                                    </ul>
+                                    <div className={styles.switchContainer}>
+                                        <label className={styles.switch}>
+                                            <input type="checkbox" />
+                                            <span className={styles.slider}></span>
+                                        </label>
+                                    </div>
+                                </>
+                            )
+                        }
+                        {
+                            renderSidebar &&
+                            <img id={styles.iconMenu} src="/src/assets/icons/menu.svg" onClick={handleSidebar} />
+                        }
+                    </div>
+                </nav>
+            </header>
+            {
+                renderSidebar && (
+                    <Sidebar showSidebar={showSidebar} handleSidebar={handleSidebar} />
+                )
+            }
+        </>
     );
 }
 
---
+++ src/constants/breakpoints.ts
@@ -0,0 +1,9 @@
+const SM: number = 768;
+const MD: number = 1024;
+const LG: number = 1280;
+
+export default {
+    SM,
+    MD,
+    LG
+};
